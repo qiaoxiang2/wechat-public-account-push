e71f0000573600490301137db023b39993fa2a87
qiaoxiang2 <116953988+qiaoxiang2@users.noreply.github.com>
2022-12-09 18:28:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,7 +49,10 @@ const USER_CONFIG = {
     /** 生日消息和节日消息 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     birthdayMessage: true,
-
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },
   PROVINCE: '内蒙古',
   CITY: '赤峰',
 
