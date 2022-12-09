95df67a0deb717b5ffe6e2ab6628adf60edeb2b3
qiaoxiang2 <116953988+qiaoxiang2@users.noreply.github.com>
2022-12-09 19:33:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -100,7 +100,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'sEXxzHwP2Vp70XmYvjRFVpsyi8t_wgBXKtakoRXkP1s',
+  CALLBACK_TEMPLATE_ID: 'ZRIv_FClBibZvrpoJq4CYeuOhYMJjGi0WAMOPAmA8S0',
 
   CALLBACK_USERS: [
     {
