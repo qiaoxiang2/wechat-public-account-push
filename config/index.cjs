ae66f22852bda99ad02d5dce9b7646eedb639e52
qiaoxiang2 <116953988+qiaoxiang2@users.noreply.github.com>
2022-12-09 11:34:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,9 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-10-17' },
-       note_ch.DATA
-       note_en.DATA
+        { keyword: 'love_day', date: '2022-10-17' }, 
       ],
     },
   ],
