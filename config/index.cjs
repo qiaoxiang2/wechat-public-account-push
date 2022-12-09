32dd8957e966fc7b10c127b8caf56c2318a1c4d5
qiaoxiang2 <116953988+qiaoxiang2@users.noreply.github.com>
2022-12-09 11:45:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oabhU6q_XMHZba54YSwR7loKcfiE',
+      id: 'oabhU6q_XMHZba54YSwR7loKcfiE','oabhU6nYnStIAICbPtDFj1ML7nFw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'AU86RkMIwc6NsMW2UArdp7B4iKtlhGtWysuvCTGJDx4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
