3a51298f988ad48325f35bec2df6976c63cdc5c5
qiaoxiang2 <116953988+qiaoxiang2@users.noreply.github.com>
2022-12-09 18:38:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,7 +63,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oabhU6nYnStIAICbPtDFj1ML7nFw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'hkv3d8yxP5q35HU8GHjhVR_id5YkmuRrmx8bnyA1uIc',
+      useTemplateId: 'wgrI2UTU2o_74vrFH_NkvcTAy7Kd5GPqyBEObNJVAqM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-01',   
       festivals: [
