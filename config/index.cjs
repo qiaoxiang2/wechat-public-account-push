395892a90fb6c472e1f91577deddac3f48601498
qiaoxiang2 <116953988+qiaoxiang2@users.noreply.github.com>
2022-12-09 12:07:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝''乔乔',
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '["oabhU6nYnStIAICbPtDFj1ML7nFw","oabhU6q_XMHZba54YSwR7loKcfiE"]',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
