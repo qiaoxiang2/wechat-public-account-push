bb595c901736f7b968648392b535a4eb693e24c9
qiaoxiang2 <116953988+qiaoxiang2@users.noreply.github.com>
2022-12-09 19:46:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -59,13 +59,13 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝'
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oabhU6nYnStIAICbPtDFj1ML7nFw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'sEXxzHwP2Vp70XmYvjRFVpsyi8t_wgBXKtakoRXkP1s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '05-01',   
+      horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -74,23 +74,16 @@ const USER_CONFIG = {
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '*生日', name: '乔乔', year: '2001', date: '11-09',
-        },
-          ],
-        
-      /**
-     * 限制重要节日的展示条目, 需要填写数字;
-     * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
-     * 如果为0, 则默认展示全部
-     */
-     festivals_limt: 3,
-      
+        }, 
+      ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-10-17' },  
+        { keyword: 'love_day', date: '2022-09-08' },  
       ],
     },
   ],
+
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
